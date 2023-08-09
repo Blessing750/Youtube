7eb00948fd568c008a88d904957db27306437207
Josie Ajose <ajosejoshua8@gmail.com>
2023-08-09 13:29:04 +0100

    More changes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Categories from './components/Categories';
 import Header from './components/Header';
+import FeaturedVideos from './components/FeaturedVideos';
 // import Video from './components/Video';
 
 function App() {
@@ -20,33 +21,7 @@ function App() {
   <Categories/>
   </div>
 
-<div className="row">
-  <div className="col-md-1"></div>
-  <div className="col-md-4">
-    <iframe width="340" height="250" src="https://www.youtube.com/embed/Mw_5eEoPIHk"
-     title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
-     clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-     allowfullscreen className="nay"></iframe>
-     <h6 className="come">Kizz Daniel X Asake</h6>
-     <h6 className="come">900 views</h6>
-    </div>
-    <div className="col-md-3">
-     <iframe width="350" height="250" src="https://www.youtube.com/embed/M6326knSuD0"
-      title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
-       clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-       allowfullscreen className="note"></iframe>
-       <h6 className="come">THE INHERITANCE Kiekie </h6>
-       <h6 className="come">1M views</h6>
-      </div>
-      <div className="col-md-4">
-      <iframe width="340" height="250" src="https://www.youtube.com/embed/aDIkv6FHPOM"
-       title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
-       clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-       allowfullscreen className="notess"></iframe>
-       <h6 className="come">Asake X Zinolesky X Naira</h6>
-       <h6 className="come">600 views</h6>
-      </div>
-</div>
+<FeaturedVideos/>
 <hr />
 <div className="row">
   <div className="col-md-1"></div>
